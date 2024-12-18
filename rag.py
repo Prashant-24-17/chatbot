da2af621b8fc837a3154308501cd808a44fbb7eb
Prashant-24-17 <prashant0770singh@gmail.com>
2024-12-18 19:51:33 +0530

    update libraries

---
+++ rag.py
@@ -0,0 +1,36 @@
+import pdf2image
+from PIL import Image
+import  pytesseract
+
+doc_img = pdf2image.convert_from_path(r"D:\nvidia_dataaset.pdf" , poppler_path=r"C:\Program Files (x86)\poppler-24.08.0\Library\bin")
+# doc_img[0].show()
+
+doc_text = []
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+for page in doc_img:
+    text = pytesseract.image_to_string(page)
+    doc_text.append(text)
+    
+    
+print(doc_text[20])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
